79874e95431ec9fbd8c1e6493f9f4a3205342378
Laura_Villalobos <lvvillalobos@uninorte.edu.co>
2025-10-09 12:42:30 -0500

    inicio mas o menos

--- inicio/inicio.gd
+++ inicio/inicio.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-
 func _on_exit_button_pressed() -> void:
+	AudioManager.SFXPlayer.stream = preload("res://inicio/audio/button-305770.mp3")
+	AudioManager.SFXPlayer.play()
 	get_tree().quit()
-	pass # Replace with function body.
