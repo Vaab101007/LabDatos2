1cee5fd41a56f4d338657d78298e4100b972f015
Laura_Villalobos <lvvillalobos@uninorte.edu.co>
2025-10-12 00:18:02 -0500

    ultimos cambios

--- inicio/scripts/again.gd
+++ inicio/scripts/again.gd
@@ -4,10 +4,4 @@ extends Node2D
 func _on_again_button_pressed() -> void:
 	AudioManager.SFXPlayer.stream = preload("res://inicio/audio/button-305770.mp3")
 	AudioManager.SFXPlayer.play()
-	SceneTransitions.change_scene_to_file("res://scenes/mapa/mapa.tscn")
-
-
-func _on_again_button_focus_entered() -> void:
-	AudioManager.SFXPlayer.stream = preload("res://inicio/audio/button-305770.mp3")
-	AudioManager.SFXPlayer.play()
-	get_tree().quit()
+	SceneTransitions.change_scene_to_file("res://inicio/inicio.tscn")
